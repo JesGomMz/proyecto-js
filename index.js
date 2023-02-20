4ea251a050147d23fb0ed7d946df8c480ea58f3b
Jesus <gearbardak@gmail.com>
2023-02-20 02:25:44 -0600

    nuevos cometnarios

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const fn = () => {
     const miString = "soy un string!"
     let b = miString + " muy feliz"
     console.log(b)
+    console.log('Hola mundo');
+    
 }
 
 fn()
 
 const clickeado = () => {
     let a = "chanchito feliz"
-
+    
     console.log(a)
 }
